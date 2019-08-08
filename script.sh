a9cdb5606a57bb09886f8749ae3159939414ae54
Matt Simmons <msimmons@spacex.com>
2019-08-08 07:49:08 -0700

    Improved syntax

--- script.sh
+++ script.sh
@@ -4,67 +4,68 @@
 # date: Aug 7th, 2019 for Mojave 10.14.6
 
 RESULTS="/Users/aa507426/Desktop/PCI_Results"
+RESULTS="${PWD}/out.txt"
+
 
 AWK_Args="'FNR <=2'"
 
-Touch $RESULTS
+touch $RESULTS
 
-Echo "Below are the PCI findings." >> $RESULTS
+echo "Below are the PCI findings." >> $RESULTS
 
-Echo "*** CURRENT ACTIVE PROCESSES ****" >> $RESULTS
+echo "*** CURRENT ACTIVE PROCESSES ****" >> $RESULTS
 
 ps aux >> $RESULTS
 
-Echo "**** STARTUP FOLDER ITEMS ****" >> $RESULTS
+echo "**** STARTUP FOLDER ITEMS ****" >> $RESULTS
 
 ls /System/Library/StartupItems  >> $RESULTS
 
-Echo "**** ACTIVE PORTS ****" >> $RESULTS
+echo "**** ACTIVE PORTS ****" >> $RESULTS
 
 netstat -a | grep ESTABLISHED  >> $RESULTS
 
-Echo "**** INSTALLED APPLICATIONS WITH VESION ****" >> $RESULTS
+echo "**** INSTALLED APPLICATIONS WITH VESION ****" >> $RESULTS
 
 find / -iname *.app >> $RESULTS
 
-Echo "**** SECURITY UPDATES ****" >> $RESULTS
+echo "**** SECURITY UPDATES ****" >> $RESULTS
 
 sw_vers -productVersion >> $RESULTS
 
- Echo "**** OS VERSION ****" >> $RESULTS
+echo "**** OS VERSION ****" >> $RESULTS
 
 sw_vers >> $RESULTS
 
-Echo "**** FIREWALL RULES ****" >> $RESULTS
+echo "**** FIREWALL RULES ****" >> $RESULTS
 
- /usr/libexec/ApplicationFirewall/socketfilterfw --listapps >> $RESULTS
+/usr/libexec/ApplicationFirewall/socketfilterfw --listapps >> $RESULTS
 
-Echo "**** AUTOLOGIN SETTINGS ****" >> $RESULTS
+echo "**** AUTOLOGIN SETTINGS ****" >> $RESULTS
 
 defaults read /Library/Preferences/com.apple.loginwindow >> $RESULTS
 
-Echo "**** SCREEN SAVER SETTINGS ****" >> $RESULTS
+echo "**** SCREEN SAVER SETTINGS ****" >> $RESULTS
 
 osascript -e 'tell application "System Events" to tell security preferences to get require password to wake'  >> $RESULTS
 
-Echo "**** NTP SETTING ****" >> $RESULTS
+echo "**** NTP SETTING ****" >> $RESULTS
 
 cat /etc/ntp.conf  >> $RESULTS
 
-Echo "**** LAST LOGIN DATE AND USER ****" >> $RESULTS
+echo "**** LAST LOGIN DATE AND USER ****" >> $RESULTS
 
 last | awk 'FNR <=2' >> $RESULTS
 
-Echo "**** HOSTNAME ****" >> $RESULTS
+echo "**** HOSTNAME ****" >> $RESULTS
 
 scutil --get LocalHostName >> $RESULTS
 
-Echo "**** NETWORK INTERFACE WITH IPS ****" >> $RESULTS
+echo "**** NETWORK INTERFACE WITH IPS ****" >> $RESULTS
 
-Ifconfig >> $RESULTS
+ifconfig >> $RESULTS
 
-Echo "**** LOCAL USER ACCOUNTS ****" >> $RESULTS
+echo "**** LOCAL USER ACCOUNTS ****" >> $RESULTS
 
 dscl . list /Users | grep -v ‘_’ >> $RESULTS
-
 
